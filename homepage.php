c5f1029f10c0d940a5cc5bfa443f44a5b6546ab5
kcsamson <158136389+kcsamson@users.noreply.github.com>
2024-05-22 18:41:11 +0800

    Add files via upload

---
+++ homepage.php
@@ -0,0 +1,93 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>IMS Homepage - Inventory Management System</title>
+    <link rel="stylesheet" type="text/css" href="login.css">
+    <script src="https://use.fontawesome.com/0c7a3095b5.js"></script>
+</head>
+<body>
+    <div class="header">
+	    <div class="homepageContainer">
+			<a href="index.php">Login</a>
+		</div>
+	</div>
+    <div class="banner">
+        <div class="homepageContainer">
+            <div class="bannerHeader">
+                <h1>CPIS</h1>
+                <p>Computer Peripherals Inventory System</p>
+            </div>
+            <p class="bannerTagline">
+                Tracks your goods throughout your entire supply chaine, from purchasing to production to end sales
+            </p>
+            <div class="bannerIcons">
+                <a href=""><i class="fa fa-apple"></i></a>
+                <a href=""><i class="fa fa-andriod"></i></a>
+                <a href=""><i class="fa fa-windows"></i></a>
+            </div>
+            </div>
+            </div>
+            <div class="homepageContainer">
+            <div class="homepageFeatures">
+                <div class="homepageFeatures">
+                <span class="featureIcon"><i class="fa fa-gear"></i></span>
+                <h3 class="featureTitle">Editable Theme</h3>
+                <p class="featureDescription">blablablabla</p>
+            </div>
+			<div class="homepageFeatures">
+                <span class="featureIcon"><i class="fa fa-star"></i></span>
+                <h3 class="featureTitle">Flat Design</h3>
+				<p class="featureDescription">blablablabla</p>
+            </div>
+            <div class="homepageFeatures">
+                <span class="featureIcon"><i class="fa fa-globe"></i></span>
+                <h3 class="featureTitle">Reach Your Audience</h3>
+				<p class="featureDescription">blablablabla</p>
+            </div>
+         </div>
+         </div>
+         <div class="homepageNotified">
+        <div class="homepageContainer">
+            <div class="homepageNotifiedContainer">
+            <div class="emailForm">
+            <h3>Get Notified of Any Updates!</h3>
+                <p>A B C D E F G H I J K L M N O P Q R S T U V W X Y Z. A B C D E F G H I J K L M N O P Q R S T U V W X Y Z. A B C D E F G H I J K L M N O P Q R S T U V W X Y Z. A B C D E F G H I J K L M N O P Q R S T U V W X Y Z. A B C D E F G H I J K L M N O P Q R S T U V W X Y Z.</p>
+            <form action="">
+                <div class="formContainer">
+                    <input type="text" placeholder="Email Address" />
+                    <button>Notify</button>
+                </div>
+            </form>
+        </div>
+        <div class="video">
+            <iframe src="https://www.youtube.com/embed/aqe1mp7yEm8?si=oqehSGZWBYNI_cm9" frameborder="0"></iframe>
+            </div>
+        </div>    
+        </div>
+     </div>
+     <div class="socials">
+        <div class="homepageContainer">
+            <h3 class="socialHeader">Say Hi & Get in Touch</h3>
+            <p class="socialText">A B C D E F G H I J K L M N O P Q R S T U V W X Y Z</p>
+            <div class="socialIconsContainer">
+                <a href=""><i class="fa fa-twitter"></i></a>
+                <a href=""><i class="fa fa-facebook"></i></a>
+                <a href=""><i class="fa fa-pinterest"></i></a>
+                <a href=""><i class="fa fa-google-plus"></i></a>
+                <a href=""><i class="fa fa-linkedin"></i></a>
+                <a href=""><i class="fa fa-youtube"></i></a>
+            </div>
+        </div>
+     </div>
+	 <div class="footer">
+        <div class="homepageContainer">
+            <a href="">Contact</a>
+            <a href="">Download</a>
+            <a href="">Press</a>
+            <a href="">Email</a>
+            <a href="">Support</a>
+            <a href="">Privacy Policy</a>
+        </div>
+	 </div>
+    </body>
+</html>

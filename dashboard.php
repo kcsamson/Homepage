e1bec6db93f616897f8e8064fe04c9b24471dad5
kcsamson <158136389+kcsamson@users.noreply.github.com>
2024-05-23 09:59:12 +0800

    Add files via upload

---
+++ dashboard.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Dashboard - Inventory Management System</title>
+    <link rel="stylesheet" type="text/css" href="login.css">
+    <script src="https://use.fontawesome.com/0c7a3095b5.js"></script>
+</head>
+<body>
+    <div id="dashboardMainContainer">
+        <div class="dashboard_sidebar">
+            <h3 class="dashboard_logo">CPIS</h3>
+            <div class="dashboard_sidebar_user">
+                <img src="images/user/360_F_587766653_PkBNyGx7mQh9l1XXPtCAq1lBgOsLl6xH.jpg" alt="User Image" />
+                <span>BSCPE</span>
+            </div>
+            <div class="dashboard_sidebar_menus">
+                <ul class="dashboard_menu_lists">
+					<li class="menuActive">
+                        <a href=""><i class="fa fa-dashboard"></i>Dashboard</a>
+					</li>
+					<li>
+                        <a href=""><i class="fa fa-dashboard"></i>Dashboard</a>
+					</li>
+                </ul>
+            </div>
+        </div>
+        <div class="dashboard_content_container">
+            <div class="dashboard_topNav">
+                <a href="" id=toggleBtn><i class="fa fa-navicon"></i></a>
+                <a href="" id="logoutBtn"><i class="fa fa-power-off"></i>Log-out</a>
+            </div>
+            <div class="dashboard_content">
+                <div class="dashboard_content_main">
+				</div>
+            </div>
+        </div>
+    </div>
+<script>
+	toggleBtn.addEvenListener('click', () => {
+		alert(hi);
+	});
+</script>
+</body>
+</html>
